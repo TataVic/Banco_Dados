e9ec222d36ba8f90b195974b8e006305e824a621
Tauani Vitória <tauanivitoria1302@gmail.com>
2023-10-05 19:31:35 -0300

    exercicios de trigger e correçoes

---
+++ Aula 5 - Triggers/Exercicio_Trigger_1.sql
@@ -0,0 +1,37 @@
+-- aula 03/10/2023 --Triggers Exercicio 1
+--1 - Após incluir uma peça com atributo cor 'Amarelo', modifique tal cor para 'Azul'.  
+Create Trigger Ex1 on Peca after insert as
+	Update Peca set PeCor = 'Azul' where PeNro in (select PeNro from inserted)  and (select PeCor from inserted) = 'Amarelo'
+	
+	insert into Peca values (8, 'Yuri',100, 'Amarelo')
+	
+	select * from Peca
+
+
+--2 - Ao invés de deletar uma peça, crie um trigger para modificar o atributo PePreco dela para 50.  
+Create trigger Ex2 on Peca INSTEAD OF delete as 
+	Update Peca set PePreco = 50 where PeNro in (select PeNro from deleted)
+	delete from Peca where PeNro = 6
+
+--3 - Crie um trigger para, ao invés de atualizar o nome de uma peça com um valor qualquer, atualize o campo Pecor dela para 'Amarelo'.  
+Create trigger Ex3 on Peca INSTEAD OF update as
+    IF UPDATE (PeNome)
+    BEGIN
+        UPDATE peca
+        SET Pecor = 'Amarelo'
+        FROM peca AS p
+        INNER JOIN inserted AS i ON p.PeNro = i.PeNro;
+    END
+
+	update Peca set PeNome = 'teste' where PeNro = 2
+		
+	select * from Peca
+
+--4 - Faça um trigger para uma inserção na tabela peça que ao invés de inserir um valor para Pecor, troque-o para 'Roxo' 
+Alter Trigger Ex4 on Peca insert as
+	Update Peca Set PeCor = 'Roxo'
+	Where PeNro in (select PeNro from inserted)
+
+	insert into Peca values (8, 'tauani',560, 'Lilas')
+	select * from Peca
+
---
+++ Aula 5 - Triggers/Exercicio_Trigger_2.sql
@@ -0,0 +1,26 @@
+--exercicio trigger 2 - 03/10
+--1 - Faça um trigger com variáveis para, ao invés de inserir uma peça com os valores que o usuário informou,
+--não deixe o preço da peça ser inferior a 5. 
+Create Trigger Exercicio1 on Peca for insert as 
+	DECLARE @preco_minimo float;
+    SET @preco_minimo = 5.00;
+		INSERT INTO Peca(id, nome, preco)
+			SELECT id, nome, preco
+			FROM inserted
+			WHERE preco >= @preco_minimo;
+
+--2 – Crie um Trigger que ao invés de excluir os fornecedores que não 
+--são das categorias ‘A’, ‘B’ e ‘C’, atualize suas categorias para ‘C’. 
+
+--3 - Ao se inserir um projeto, verificar se o custo está acima do permitido (50000). 
+--Se for, incluir com o limite de 50.000. Se não, incluir com o valor passado. 
+
+--4 - Criar uma tabela de Historico de Projetos. 
+
+--5 - Criar um trigger para atualizar a tabela Histórico de Projetos
+--que é disparado quando houver a modificação SOMENTE no atributo PDuração no Projeto. 
+
+--6 - Criar 2 tabelas: Projeto_atualizado e Projeto_antigo 
+
+--7 - Criar um trigger que armazena em uma tabela os dados atualizados do Projeto(Projeto_atualizado)
+--e em outra tabela os dados do Projeto antes de atualizar (Projeto_antigo) 
