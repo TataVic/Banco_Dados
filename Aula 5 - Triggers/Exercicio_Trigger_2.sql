661da5806216fa0184aa36a80e79b7642a321699
Tauani Vitória <tauanivitoria1302@gmail.com>
2023-10-17 21:49:54 -0300

    atualizações e cursores

--- Aula 5 - Triggers/Exercicio_Trigger_2.sql
+++ Aula 5 - Triggers/Exercicio_Trigger_2.sql
@@ -1,82 +1,140 @@
---exercicio trigger 2 - 03/10
---1 - Faça um trigger com variáveis para, ao invés de inserir uma peça com os valores que o usuário informou,
+-- aula 03/10/2023
+
+
+--1 - Faça um trigger com variáveis para, ao invés de inserir uma peça com os valores que o usuário informou, 
 --não deixe o preço da peça ser inferior a 5. 
-create trigger insercao on peca for insert 
-	as begin
-	declare @PeNro int
-	declare @PeNome varchar
-	declare @PePreco decimal
-	declare @Pecor varchar
-	set @PeNro = (select PeNro from inserted)
-	set @PeNome = (select PeNome from inserted)
-	set @PePreco = (select PePreco from inserted)
-	set @Pecor = (select PeCor from inserted)
-		if(@PePreco < 5)
-		begin 
-		insert into peca values (@PeNro, @PeNome,5,@PeCor)
-		print 'peça inferior ao valor 5'
-		end
-		else
-		insert into peca values (@PeNro, @PeNome,@PePreco,@PeCor)
-		print 'peça inserida com os parâmetros passados'
+create trigger ex1L2 on peca  instead of insert 
+as begin
+declare @PeNro int
+declare @PeNome varchar
+declare @PePreco decimal
+declare @Pecor varchar
+set @PeNro = (select PeNro from inserted)
+set @PeNome = (select PeNome from inserted)
+set @PePreco = (select PePreco from inserted)
+set @Pecor = (select PeCor from inserted)
+if(@PePreco < 5)
+begin 
+insert into peca values (@PeNro, @PeNome,5,@PeCor)
+end
+else
+insert into peca values (@PeNro, @PeNome,@PePreco,@PeCor)
 end
 
-insert into Peca values (9, 'teste', 4, 'vermelho')
-select * from peca
+
+insert Peca values (88,'fone',4,'rosa') 
+select*from peca
 
 
---2 – Crie um Trigger que ao invés de excluir os fornecedores que não 
---são das categorias ‘A’, ‘B’ e ‘C’, atualize suas categorias para ‘C’. 
-create trigger atualizacao on fornecedor instead of delete
+--2 – Crie um Trigger que ao invés de excluir os fornecedores que não são das categorias ‘A’, ‘B’ e ‘C’,
+-- atualize suas categorias para ‘C’. 
+
+create trigger ex2L2 on Fornecedor instead of delete
 as begin
 	Update Fornecedor set FCateg = 'C' 
-	Where Fornecedor.FNro in (select FNro from deleted)
-		and FCateg in ('A', 'B', 'C')
- end
- delete Fornecedor where FNro = 4
-select * from fornecedor
+		Where Fornecedor.FNro in (select FNro from deleted)
+			and FCateg in ('A', 'B', 'C')
+end
+delete from fornecedor where FCateg = 'D'
+select*from fornecedor
 
-
---3 - Ao se inserir um projeto, verificar se o custo está acima do permitido (50000). 
---Se for, incluir com o limite de 50.000. Se não, incluir com o valor passado. 
-create trigger insercao_projeto on projeto for insert as
-	Update Projeto set PCusto = 'C' 
-	Where Fornecedor.FNro in (select FNro from deleted)
-		and FCateg in ('A', 'B', 'C')
- end
- delete Fornecedor where FNro = 4
-select * from fornecedor
-
---4 - Criar uma tabela de Historico de Projetos. 
-create Table Historico_Projetos(
-	PjNro int primary key not null, 
-	PjNome varchar(255) not null, 
-	PjDuracaoNova int not null, 
-	PjDuracaoAntiga int not null, 
-	PjCusto float not null
-)
-select * from Historico_Projetos
+--3 - Ao se inserir um projeto, verificar se o custo está acima do permitido (50000). Se for, incluir com o limite de 50.000. 
+--Se não, incluir com o valor passado. 
+Create Trigger ex3L2 on Projeto after insert as begin
+	Declare @custo float 
+	SELECT @custo = PCusto FROM inserted
+IF @custo > 50000
+    BEGIN
+        UPDATE Projeto
+        SET PCusto = 50000
+        WHERE PNro IN (SELECT PNro FROM inserted)
+    END
+    ELSE
+    BEGIN
+        PRINT 'SERÁ INCLUIDO O VALOR PASSADO'
+        UPDATE Projeto
+        SET PCusto = @custo
+        WHERE PNro IN (SELECT PNro FROM inserted)
+    END
+END
+ insert into Projeto values (6, 'teste', 3, 50001) 
+  insert into Projeto values (7, 'teste', 3, 4000) 
 select * from Projeto
 
---5 - Criar um trigger para atualizar a tabela Histórico de Projetos
---que é disparado quando houver a modificação SOMENTE no atributo PDuração no Projeto. 
-Create Trigger atual_Hist_Projeto on Historico_Projetos after update as
-	 IF UPDATE(PjDuracao)
+--4 - Criar uma tabela de Historico de Projetos. 
+Create table Historico_Projeto (
+	Nro int not null PRIMARY KEY,
+    DataAlteracao DATETIME NOT NULL, 
+    PNro INT NOT NULL, 
+    CampoAlterado VARCHAR(255) NOT NULL, 
+    ValorAntigo float, 
+    ValorNovo float  
+)
+
+ALTER TABLE Historico_Projeto
+ADD CONSTRAINT FK_Historico_Projeto
+FOREIGN KEY (PNro) REFERENCES Projeto(Nro);
+
+--5 - Criar um trigger para atualizar a tabela Histórico de Projetos que é disparado quando houver a modificação SOMENTE no atributo 
+--PDuração no Projeto. 
+CREATE TRIGGER Ex5L2 ON Projeto AFTER UPDATE
+AS
+BEGIN
+    IF UPDATE(PDuracao)
     BEGIN
-        INSERT INTO Historico_Projetos (PjNro, PjNome, PjDuracaoNova, PjDuracaoAntiga, PjCusto)
-		values(
-        SELECT 
-            inserted.PjNro,
-            inserted.PjNome,
-            inserted.PjDuracao,
-            deleted.PjDuracao,
-            inserted.PjCusto
-        FROM inserted
-		)
-	end
-
-
+        INSERT INTO Historico_Projetos values (DataAlteracao, PNro, CampoAlterado, ValorAntigo, ValorNovo)
+        SELECT
+            GETDATE(),  
+            i.PNro,    
+            'Duracao alterada',
+            d.PDuracao,
+            i.PDuracao  -- Valor novo do campo (após a atualização)
+        FROM inserted i
+        JOIN deleted d ON i.PNro = d.PNro;
+    END
+END;
 --6 - Criar 2 tabelas: Projeto_atualizado e Projeto_antigo 
+Create table Projeto_atualizado (
+	[PNro_atual] [int] NOT NULL,
+	[PNome_atual] [varchar](30) NOT NULL,
+	[PDuracao_atual] [varchar](15) NOT NULL,
+	[PCusto_atual] [money] NOT NULL,
+)
+Create table Projeto_antigo (
+	[PNro_ant] [int] NOT NULL,
+	[PNome_ant] [varchar](30) NOT NULL,
+	[PDuracao_ant] [varchar](15) NOT NULL,
+	[PCusto_ant] [money] NOT NULL,
+)
 
---7 - Criar um trigger que armazena em uma tabela os dados atualizados do Projeto(Projeto_atualizado)
---e em outra tabela os dados do Projeto antes de atualizar (Projeto_antigo) 
+--7 - Criar um trigger que armazena em uma tabela os dados atualizados do Projeto(Projeto_atualizado) e 
+--em outra tabela os dados do Projeto antes de atualizar (Projeto_antigo)
+-- Trigger para armazenar dados atualizados em Projeto_atualizado
+CREATE TRIGGER Ex7L2
+ON Projeto
+AFTER UPDATE
+AS
+BEGIN
+    INSERT INTO Projeto_atualizado (PNro_atual, PNome_atual, PDuracao_atual, PCusto_atual)
+    SELECT
+        i.PNro,  
+        i.PNome,
+        i.PDuracao, 
+        i.PCusto
+    FROM inserted i
+END;
+
+-- Trigger para armazenar dados antigos em Projeto_antigo
+CREATE TRIGGER Trigger_Projeto_Antigo
+ON Projeto
+AFTER UPDATE
+AS
+BEGIN
+    INSERT INTO Projeto_antigo (PNro_ant, PNome_ant, PDuracao_ant, PCusto_ant)
+    SELECT
+        d.PNro,  
+        d.PNome, 
+        d.PDuracao, 
+        d.PCusto 
+    FROM deleted d
+END;
