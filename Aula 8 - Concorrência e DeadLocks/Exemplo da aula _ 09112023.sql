c2ed7dc0cc7381eeef78c87f360e938cc10836f9
Tauani Vitória <tauanivitoria1302@gmail.com>
2023-11-09 21:54:06 -0300

    continuação da aula

--- Aula 8 - Concorrência e DeadLocks/Exemplo da aula _ 09112023.sql
+++ Aula 8 - Concorrência e DeadLocks/Exemplo da aula _ 09112023.sql
@@ -1,5 +1,27 @@
+--Comando 1 -- Comando: Leitura Suja
 SET TRANSACTION ISOLATION LEVEL READ COMMITTED 
 GO 
 BEGIN TRANSACTION
 SELECT val FROM tbl
 COMMIT TRANSACTION 
+
+---------
+--Comando 2 - Comando: Leitura não repetível
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED 
+GO
+BEGIN TRAN repeatRead
+
+select * from tbl
+waitfor delay '00:00:05'
+select * from tbl
+commit tran repeatRead
+
+--------------------
+--Comando 3 
+Begin tran shortUpdate
+update tbl
+set val = -1
+commit tran shortUpdate
+------------
+--Comando 4
+--readetable (continuacao)
